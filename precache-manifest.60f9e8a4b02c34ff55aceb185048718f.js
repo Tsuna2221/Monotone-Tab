a824ba012268cc0d78c5ffe60f6db920ccb7d49f
Thiago <tsuna2221@live.com>
2019-01-02 14:52:12 -0200

    Updates

---
+++ precache-manifest.60f9e8a4b02c34ff55aceb185048718f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "006c474475130bfa6b61eddf270988cf",
+    "url": "/Monotone-Tab/static/media/Mail.006c4744.svg"
+  },
+  {
+    "revision": "92dffb7c4a7c895637ac",
+    "url": "/Monotone-Tab/static/css/main.a231537a.chunk.css"
+  },
+  {
+    "revision": "09790b0be8d2f45efa41",
+    "url": "/Monotone-Tab/static/js/1.09790b0b.chunk.js"
+  },
+  {
+    "revision": "1257907870476e0e2852",
+    "url": "/Monotone-Tab/static/js/runtime~main.12579078.js"
+  },
+  {
+    "revision": "223ae4401c043456b8b0ba801a312f68",
+    "url": "/Monotone-Tab/static/media/MenuButton.223ae440.svg"
+  },
+  {
+    "revision": "5f0f15006d83bf3430486946e47fa63a",
+    "url": "/Monotone-Tab/static/media/Notes.5f0f1500.svg"
+  },
+  {
+    "revision": "8e868d45ce60e36bbaf13de27842dba4",
+    "url": "/Monotone-Tab/static/media/Calendar.8e868d45.svg"
+  },
+  {
+    "revision": "3890a5fd945c2014641853e0338dd8c7",
+    "url": "/Monotone-Tab/static/media/Evernote.3890a5fd.svg"
+  },
+  {
+    "revision": "92dffb7c4a7c895637ac",
+    "url": "/Monotone-Tab/static/js/main.92dffb7c.chunk.js"
+  },
+  {
+    "revision": "f088f485efb75775b298fcf9cb6e65f0",
+    "url": "/Monotone-Tab/static/media/Info.f088f485.svg"
+  },
+  {
+    "revision": "7d2b2e0caaf30acbd79ca88074573844",
+    "url": "/Monotone-Tab/static/media/Setting.7d2b2e0c.svg"
+  },
+  {
+    "revision": "0cdb1071fe4e5b5adf73eb684b7a778c",
+    "url": "/Monotone-Tab/static/media/Expand.0cdb1071.svg"
+  },
+  {
+    "revision": "e5d1adcbfda0cefad6eaee3f57148a32",
+    "url": "/Monotone-Tab/static/media/OptionButton.e5d1adcb.svg"
+  },
+  {
+    "revision": "09e37ac1b8f86af2dc7648b7c38ff4fc",
+    "url": "/Monotone-Tab/static/media/NoteButton.09e37ac1.svg"
+  },
+  {
+    "revision": "5c382123b874142a3875263fafed6774",
+    "url": "/Monotone-Tab/static/media/NoteArrow.5c382123.svg"
+  },
+  {
+    "revision": "27264edf21f7994db7cf220289f3fa8d",
+    "url": "/Monotone-Tab/static/media/Google.27264edf.svg"
+  },
+  {
+    "revision": "73ef91989ff97d754fdc0dd1db7423fe",
+    "url": "/Monotone-Tab/index.html"
+  }
+];
